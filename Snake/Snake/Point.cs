5246e3cf15cc9776f1ad9ae279e332f804d4c2c5
HumorPlay <gabage@list.ru>
2019-09-15 23:30:42 +0300

    start

---
+++ Snake/Snake/Point.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Point
+    {
+
+        public int x { get; set; }
+        public int y { get; set; }
+        public char sym { get; set; }
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
+
